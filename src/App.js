a112e4f9021461ff26cdcba445ce66714e421e94
Semert <semert2121@gmail.com>
2020-09-10 16:15:40 +0300

    Installed axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import axios from "axios"
 
 
 https://pokeapi.co/api/v2/pokemon
 function App() {
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => console.log(res.data))
+
   return (
     <div></div>
   );
