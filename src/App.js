204c2573b7acb222772c830fd143299bed75d415
Semert <semert2121@gmail.com>
2020-09-10 16:20:52 +0300

    fetch the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios"
 
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
 
-  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => console.log(res.data.results))
+  useEffect(() => (
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => console.log(res.data.results.map(result => result.namen)))
+  ),[])
+  
 
   return (
     <div></div>
