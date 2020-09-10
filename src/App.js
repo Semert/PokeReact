c3b9ac6b2e833e455709e52e2fcd7dab9b94929b
Semert <semert2121@gmail.com>
2020-09-10 16:38:46 +0300

    useeffect

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import PokemonList from './components/PokemonList';
 function App() {
   const [pokemon, setPokemon] = useState([])
   const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl,setNextPageUrl] = useState()
+  const [prevPageUrl,setPrevPageUrl] = useState()
+  const [loading,setLoading] = useState(true)
 
-  useEffect(() => (
-    axios.get(currentPageUrl).then(res => setPokemon(res.data.results.map(p => p.name)))
-  ),[currentPageUrl])
+  useEffect(() => {
+    setLoading(true)
+    axios.get(currentPageUrl).then(res =>{
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous) 
+      setPokemon(res.data.results.map(p => p.name))
+    })
+}, [currentPageUrl])
+
+if(loading) return "Loading..."
   
   console.log(pokemon)
   return (
